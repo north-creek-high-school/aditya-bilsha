6eabe3b19aa9fe5b0b410bd53601f5960a42d7bb
2000523 <2000523@NCHS120490505.ms.nsd.org>
2019-11-22 09:37:11 -0800

    Finished??

--- BrainCell.java
+++ BrainCell.java
@@ -4,26 +4,38 @@ public class BrainCell {
 	
 	//positions contains all the positions for the AI to place its mark
 	//AI learns by adding the same position repeatedly or removing a certain position
-	public Set<List<Integer>> positions = new HashSet<>();
+	public Set<Integer> positions = new HashSet<>();
 	
 	/**
 	 * Creates a Braincell with a starting set of all the possible positions for the AI
 	 * to place its move relative to the specific board state
-	 * @param boardState
+	 * @param boardState [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 	 */
 	public void Braincell(int[][] boardState) {
+		int position = 0;
 		for (int row = 0; row < boardState.length; row++) {
 			for (int col = 0; col < boardState[row].length; col++) {
 				if (boardState[row][col] == 0); {
-					positions.add(Arrays.asList(row, col));
+					positions.add(position);
 				}
+				position++;
 			}
 		}
 	}
 	
-	public void add() {
-		
+	//add the moves into braincell to increase likelihood of being picked again
+	public void addMove(int move) {
+		positions.add(move);
 	}
 	
+	//remove the moves into braincell to decrease likelihood of being picked again
+	public void removeMove(int move) {
+		if (positions.contains(move)) {
+			positions.remove(move);
+		}
+		if (!positions.contains(move)) {
+			positions.add(move);
+		}
+	}
 	
 }
