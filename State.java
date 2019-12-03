133befb532f5f40e6dbde49d1e684b870fad81ec
adkris2002 <56169740+adkris2002@users.noreply.github.com>
2019-12-02 21:07:41 -0800

    Commented the bugs

--- State.java
+++ State.java
@@ -20,8 +20,10 @@ public class State {
 	//goes through region array and creates a state array
 	//inside a state object that is exactly the same, returns state object
 	public State getCurrentState() {
-		BoardBrain b = new BoardBrain(); 
-		int[][] region = b.region; 
+		BoardBrain b = new BoardBrain(); //<-- Whenever you make a new BoardBrain,
+		//the region is a completely new region than the one that is meant for the game
+		int[][] region = b.region; //<-- b.region is full of null values, 
+		//the int[][] that represents the game board is in TicTacToeBoard called boardBrain.region
 		State s = new State(); 
 		int[][] st = s.state; 
 		for (int row = 0; row < region.length; row++) { 
